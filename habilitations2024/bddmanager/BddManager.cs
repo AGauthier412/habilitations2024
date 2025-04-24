e440329e4fe5a64aeba65667ee27277242a7338a
Gauthier AMBROISE <gauthier.ambroise2002@gmail.com>
2025-04-24 19:56:42 +0200

    Phase1 : Création des paquetages et classes, début de code de bddmanager

---
+++ habilitations2024/bddmanager/BddManager.cs
@@ -0,0 +1,53 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace habilitations2024.bddmanager
+{
+    public class BddManager
+    {
+        // instance unique de la classe
+        private static BddManager instance = null;
+
+        // objet de connexion à la BDD à partir d'une chaîne de connexion
+        private readonly MySqlConnection connection;
+
+        // Constructeur pour créer la connexion à la BDD et l'ouvrir
+        private BddManager(string stringConnect)
+        {
+            connection = new MySqlConnection(stringConnect);
+            connection.Open();
+        }
+
+        // Création d'une seule instance de la classe
+        public static BddManager GetInstance(string stringConnect)
+        {
+            if (instance == null)
+            {
+                instance = new BddManager(stringConnect);
+            }
+
+            return instance;
+        }
+
+        // Exécution d'une requête autre que "selec"
+        public void ReqUpdate(string stringQuery, Dictionary<string, object> parameters = null)
+        {
+            MySqlCommand command = new MySqlCommand(stringQuery, connection);
+            if (!(parameters == null))
+            {
+                foreach (KeyValuePair<string, object> parameter in parameters)
+                {
+                    command.Parameters.Add(new MySqlParameter(parameter.Key, parameter.Value));
+                }
+            }
+
+            command.Prepare();
+            command.ExecuteNonQuery();
+        }
+    }
+}
--- habilitations2024/view/FrmHabilitations.cs
+++ habilitations2024/view/FrmHabilitations.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
-namespace habilitations2024
+namespace habilitations2024.view
 {
-    public partial class Form1 : Form
+    public partial class FrmHabilitations : Form
     {
-        public Form1()
+        public FrmHabilitations()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
         }
     }
 }
